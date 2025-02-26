f1ef1ffb3340525761bfa3d90050eea4ee1f2302
adTe1 <001adte@gmail.com>
2025-02-26 14:23:12 +0200

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const prodRouter = require(`./routes/prodRoutes`);
 const userRouter = require(`./routes/userRoutes`);
 const reviewRouter = require('./routes/reviewRoutes');
-const cartRouter = require('./routes/cartRoutes');
+//const cartRouter = require('./routes/cartRoutes');
 const orderRouter = require('./routes/orderRoutes');
 const uploadRouter = require('./routes/uploadRoutes');
 
@@ -158,7 +158,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/products', prodRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/cart', cartRouter);
+//app.use('/api/v1/cart', cartRouter);
 app.use('/api/v1/orders', orderRouter);
 app.use('/api/v1/upload', uploadRouter);
 
